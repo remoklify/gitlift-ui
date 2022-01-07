c5b178ed193e74d6503b6e20ff5840a55fc95cee
fatihturker <fatih.turker@optiva.com>
2022-01-07 16:41:15 +0300

    fix search box

--- src/app/page/search-page/search-page/search-page.component.ts
+++ src/app/page/search-page/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
+  faSearch = faSearch;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  search = (event: any) => {
+    console.log(event.target.value);
+  }
 }
