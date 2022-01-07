855eb3336ff38137a1a9f2ecd7b8b99dfa2055ed
fatihturker <fatih.turker@optiva.com>
2022-01-07 18:17:20 +0300

    enhance media queries

--- src/app/page/search-page/search-page/search-page.component.ts
+++ src/app/page/search-page/search-page/search-page.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.scss'],
 })
 export class SearchPageComponent implements OnInit {
-
   faSearch = faSearch;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   search = (event: any) => {
     console.log(event.target.value);
-  }
+    if (event && event.target && event.target.value) {
+      this.router.navigate(['/user/' + event.target.value]).then(() => {
+        return true;
+      });
+    }
+  };
 }
