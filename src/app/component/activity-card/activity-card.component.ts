0c4e025b88e9de7ccf3589ea58b240c6f2d7552d
fatihturker <fatih.turker@optiva.com>
2022-01-10 19:14:16 +0300

    enhance canvas

--- src/app/component/activity-card/activity-card.component.ts
+++ src/app/component/activity-card/activity-card.component.ts
@@ -14,26 +14,120 @@ export class ActivityCardComponent implements OnInit {
 
   private context: CanvasRenderingContext2D;
 
+  private width: number = 500;
+  private height: number = 200;
+  private padding: number = 50;
+  private font: string = '12pt Calibri';
+  private x: number = 0;
+  private y: number = 0;
+  private maxX: number = 0;
+  private maxY: number = 0;
+
   constructor() {}
 
   ngOnInit(): void {
-    if (this.activityChart) {
+    if (this.activityChart && this.contributionDays) {
       console.log(this.activityChart);
       this.context = (
         this.activityChart.nativeElement as HTMLCanvasElement
       ).getContext('2d') as CanvasRenderingContext2D;
-      this.draw();
+      this.draw(this.contributionDays);
     }
   }
 
-  private draw() {
+  getLongestValueWidth = (length: number) => {
+    var longestValueWidth = 0;
+    for (var n = 0; n <= length; n++) {
+      var value = (this.maxY - n * length).toString();
+      longestValueWidth = Math.max(
+        longestValueWidth,
+        this.context.measureText(value).width
+      );
+    }
+    return longestValueWidth;
+  };
+
+  draw = (contributionDays: Activity[]) => {
     const chart: HTMLCanvasElement = this.activityChart.nativeElement;
-    chart.width = 500;
-    chart.height = 200;
-    this.context.fillStyle = 'rgba(136, 14, 79, 0.2)';
+    chart.width = this.width;
+    chart.height = this.height;
+    this.context.fillStyle = 'rgba(245, 127, 23, 0.2)';
 
     this.context.fillRect(0, 0, chart.width, chart.height);
-  }
+
+    const max = this.getMaxContribution(contributionDays);
+
+    this.x =
+      this.getLongestValueWidth(contributionDays.length) + this.padding * 2;
+    this.y = this.padding * 2;
+
+    this.drawXAxis(this.context, contributionDays, 5);
+
+    // for (let i = 0; i < contributionDays.length; i++) {
+    //   const act = contributionDays[i];
+    //   // let toY =
+    //   //   y + (act.contributionCount / max) * (this.height - this.padding);
+    //   // let toX = x + this.width / contributionDays.length;
+    //   // this.drawLine(this.context, x, y, toX, toY);
+    //   // y = toY;
+    //   // x = toX;
+    // }
+  };
+
+  drawLine = (
+    ctx: CanvasRenderingContext2D,
+    fromX: number,
+    fromY: number,
+    toX: number,
+    toY: number
+  ) => {
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    ctx.strokeStyle = '#d2cece';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.translate(toX, toY);
+  };
+
+  drawXAxis = (
+    ctx: CanvasRenderingContext2D,
+    contributionDays: Activity[],
+    tickSize: number
+  ) => {
+    let x = 0;
+    let y = this.height - tickSize;
+    for (var n = 0; n < contributionDays.length; n++) {
+      const act = contributionDays[n];
+      x = (n + 1) * (this.width / contributionDays.length);
+      this.drawLine(ctx, x, y, x, y + tickSize);
+      this.drawText(ctx, act.weekday, x, y);
+    }
+  };
+
+  drawText = (
+    ctx: CanvasRenderingContext2D,
+    weekday: number,
+    x: number,
+    y: number
+  ) => {
+    ctx.lineWidth = 0.5;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.strokeText(this.getDay(weekday), 80, 10);
+  };
+
+  getMaxContribution = (contributionDays: Activity[]) => {
+    let max = 0;
+    for (let i = 0; i < contributionDays.length; i++) {
+      const act = contributionDays[i];
+      if (act.contributionCount > max) {
+        max = act.contributionCount;
+      }
+    }
+
+    return max;
+  };
 
   getDay = (weekday: number) => {
     let day: string = '';
