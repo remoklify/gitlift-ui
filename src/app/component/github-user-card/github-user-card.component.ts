ecbef4686139c0868894c092a9c93a5a0063359b
Fatih Turker <fatihturker35@gmail.com>
2022-01-21 18:48:11 +0300

    enhance contribution card

--- src/app/component/github-user-card/github-user-card.component.ts
+++ src/app/component/github-user-card/github-user-card.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
 import { GithubUser } from 'src/app/model/github-user/github-user.model';
 import { GithubService } from 'src/app/service/github/github.service';
 import { Location } from '@angular/common';
+import { documentToSVG, elementToSVG, inlineResources } from 'dom-to-svg';
 
 @Component({
   selector: 'app-github-user-card',
@@ -11,6 +12,9 @@ import { Location } from '@angular/common';
 })
 export class GithubUserCardComponent implements OnInit {
   @Input() username: string = '';
+
+  @ViewChild('mark') mark: ElementRef;
+
   public user: GithubUser = {} as GithubUser;
   public loading: boolean = true;
   public userFound: boolean = false;
@@ -51,5 +55,19 @@ export class GithubUserCardComponent implements OnInit {
 
   back = () => {
     this.location.back();
+  };
+
+  integrate = async () => {
+    const svgDocument = documentToSVG(document);
+
+    // Inline external resources (fonts, images, etc) as data: URIs
+    await inlineResources(svgDocument.documentElement);
+
+    // Get SVG string
+    const svgString = new XMLSerializer().serializeToString(svgDocument);
+
+    const file = new Blob([svgString], { type: '.svg' });
+    const url = URL.createObjectURL(file);
+    window.open(url);
   };
 }
